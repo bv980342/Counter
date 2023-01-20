bbbd4a67d2d91dc535e9806afcb7a332e6469ded
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-01-21 00:54:37 +0300

    Creating project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+
 
 function App() {
+  let [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="counter">
+        <div className="plus" onClick={() => setCount(count = count + 1)}>Increment</div>
+        <div className="number">{count}</div>
+        <div className="minus" onClick={() => setCount(count = count - 1)}>Decrement</div>
+      </div>
     </div>
   );
 }
